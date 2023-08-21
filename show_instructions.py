5140f4349a3ac4de6ace105c14e15cf3cb191f3e
Cylus123 <134886377+Cylus123@users.noreply.github.com>
2023-08-21 00:35:15 +0000

    added comments to ensure I know what I am doing and keeping on track

--- show_instructions.py
+++ show_instructions.py
@@ -1,15 +1,22 @@
+# Define a function to get a yes or no response from the user
 def yes_no(question):
     while True:
+        # Prompt the user and convert their response to lowercase and strip whitespace
         reply = input(question).strip().lower()
         if reply.startswith('y'):
+            # If the response starts with 'y', return True
             return True
         elif reply.startswith('n'):
+            # If the response starts with 'n', return False
             return False
         else:
+            # If the response is not 'yes' or 'no', prompt the user again
             print("Please enter 'yes' or 'no'.")
 
-show_instructions = yes_no("Have you used the Area/Perimeter tool before ?: ")
+# Get the user's response to whether they want to see instructions
+show_instructions = yes_no("Do you want to see instructions? ")
 
+# Check if the user's response is 'n' (no) and display instructions accordingly
 if not show_instructions:
     print("**************************")
     print("        INSTRUCTIONS      ")
@@ -19,6 +26,7 @@ if not show_instructions:
     print("3. Enter the required measurements for the selected shape.")
     print("4. Your calculations will be displayed in a table when you finish.")
     print("**************************")
+
 
 
 
