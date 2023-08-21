5758af95a5f7b1785acdeb5055c8c0ec16ec4e79
Cylus123 <134886377+Cylus123@users.noreply.github.com>
2023-08-21 10:43:32 +0000

    added a way to exit out of the program to the instructions so the user knows how to leave when wanted.

--- show_instructions.py
+++ show_instructions.py
@@ -1,7 +1,6 @@
 # Define a function to get a yes or no response from the user
 def yes_no(question):
     while True:
-        # Prompt the user and convert their response to lowercase and strip whitespace
         reply = input(question).strip().lower()
         if reply.startswith('y'):
             # If the response starts with 'y', return True
@@ -14,7 +13,7 @@ def yes_no(question):
             print("Please enter 'yes' or 'no'.")
 
 # Get the user's response to whether they want to see instructions
-show_instructions = yes_no("Do you want to see instructions? ")
+show_instructions = yes_no("Have you used the Area/Perimeter tool before? : ")
 
 # Check if the user's response is 'n' (no) and display instructions accordingly
 if not show_instructions:
@@ -24,7 +23,9 @@ if not show_instructions:
     print("1. Enter your name.")
     print("2. Choose a shape to calculate: triangle, square, rectangle, circle, or parallelogram.")
     print("3. Enter the required measurements for the selected shape.")
-    print("4. Your calculations will be displayed in a table when you finish.")
+    print("4.Your calculations will be displayed in a table when you finish")
+
+    print('If you decide that you are finished with all your calculations type in the code "xxx" and it will automaticlly exit you out of the program!')
     print("**************************")
 
 
