d789e1ee23c5b0561178cbca7d285fbe5d05fd5a
Cylus123 <134886377+Cylus123@users.noreply.github.com>
2023-08-21 22:41:22 +0000

    changed  it from startswith and the first letters to the first 3 letters to ensure accuracy

--- shape_options.py
+++ shape_options.py
@@ -1,31 +1,27 @@
 while True:
-    reply = input("What shape would you like to calculate ?: ").strip().lower()
-    if reply.startswith('t'):
-        # If the response starts with 't', that means they want triangle
-      print("The formula for Area of a triangle is A = 1/2 × b ×  h"  )         
-      triangle_formula_base = int(input("What would you like to enter for B(base)"))
-      triangle_formula_base2 = int(input("What would you like to enter for H(height)"))
+    reply = input("What shape would you like to calculate?: ").strip().lower()
+    
+    if reply.startswith('tri'):
+        # If the response starts with 'tri', that means they want a triangle
+        print("The formula for Area of a triangle is A = 1/2 × b × h")
       
-    elif reply.startswith('c'):
-      print("The formula for Area of a circle is A = π × r^2"  )              
-      circle_formula_radius = int(input("What would you like to enter for R(radius)"))
-
-    elif reply.startswith('s'):
-      print("The formula for Area of a square is A = a^2"  )              
-     square_formula_area = int(input("What would you like to enter for A()"))
-     elif reply.startswith('p'):
-      print("The formula for Area of a parallelogram is A = b x h"  )              
-       parallelogram_formula_base = int(input("What would you like to enter for B(base)"))
-      parallelogram_formula_base2 = int(input("What would you like to enter for H(height)"))
-
-     elif reply.startswith('r'):
-      print("The formula for Area of a parallelogram is A = L x W"  )              
-       parallelogram_formula_base = int(input("What would you like to enter for L(length)"))
-      parallelogram_formula_base2 = int(input("What would you like to   enter for W(width)"))
-
-
-        # If the response starts with 'n', 
+    elif reply.startswith('cir'):
+        # If the response starts with 'cir', that means they want a circle
+        print("The formula for Area of a circle is A = π × r^2")
+        
+    elif reply.startswith('squ'):
+        # If the response starts with 'squ', that means they want a square
+        print("The formula for Area of a square is A = a^2")
+        
+    elif reply.startswith('par'):
+        # If the response starts with 'par', that means they want a parallelogram
+        print("The formula for Area of a parallelogram is A = b x h")
+        
+    elif reply.startswith('rec'):
+        # If the response starts with 'rec', that means they want a rectangle
+        print("The formula for Area of a rectangle is A = L x W")
         
     else:
-        # If the response is not 'yes' or 'no', prompt the user again
-        print("I apoligise but the Area/Perimeter tool only knows 5 shapes right now, please enter one of the shapes provided: ")
+        # If the response is not one of the recognized shapes, prompt the user again
+        print("I apologize, but the Area/Perimeter tool only knows 5 shapes right now. Please enter one of the shapes provided.")
+
