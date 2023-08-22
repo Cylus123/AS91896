5b32c2dc2fc5f154057a147aca5a60af2f3aef5f
Cylus123 <134886377+Cylus123@users.noreply.github.com>
2023-08-22 23:52:55 +0000

    made it so that if the user enters xxx then they can then leave the code

--- shape_options.py
+++ shape_options.py
@@ -1,27 +1,36 @@
 while True:
-    reply = input("What shape would you like to calculate?: ").strip().lower()
-    
-    if reply.startswith('tri'):
-        # If the response starts with 'tri', that means they want a triangle
-        print("The formula for Area of a triangle is A = 1/2 × b × h")
-      
-    elif reply.startswith('cir'):
-        # If the response starts with 'cir', that means they want a circle
-        print("The formula for Area of a circle is A = π × r^2")
-        
-    elif reply.startswith('squ'):
-        # If the response starts with 'squ', that means they want a square
-        print("The formula for Area of a square is A = a^2")
-        
-    elif reply.startswith('par'):
-        # If the response starts with 'par', that means they want a parallelogram
-        print("The formula for Area of a parallelogram is A = b x h")
-        
-    elif reply.startswith('rec'):
-        # If the response starts with 'rec', that means they want a rectangle
-        print("The formula for Area of a rectangle is A = L x W")
-        
+    reply = input("What shape would you like to calculate? (Enter 'xxx' to exit): ").strip().lower()
+
+    if reply == 'xxx':
+        print("Exiting the program.")
+        break  # Exit the loop if the user enters 'xxx'
+
+    if reply.isalpha():
+        if reply.startswith('t'):
+            # If the response starts with 't', that means they want a triangle
+            print("The formula for Area of a triangle is A = 1/2 × b × h")
+
+        elif reply.startswith('c'):
+            # If the response starts with 'c', that means they want a circle
+            print("The formula for Area of a circle is A = π × r^2")
+
+        elif reply.startswith('s'):
+            # If the response starts with 's', that means they want a square
+            print("The formula for Area of a square is A = a^2")
+
+        elif reply.startswith('p'):
+            # If the response starts with 'p', that means they want a parallelogram
+            print("The formula for Area of a parallelogram is A = b x h")
+
+        elif reply.startswith('r'):
+            # If the response starts with 'r', that means they want a rectangle
+            print("The formula for Area of a rectangle is A = L x W")
+
+        else:
+            # If the response is not one of the recognized shapes, prompt the user again
+            print("I apologize, but the Area/Perimeter tool only knows 5 shapes right now. Please enter one of the shapes provided.")
     else:
-        # If the response is not one of the recognized shapes, prompt the user again
-        print("I apologize, but the Area/Perimeter tool only knows 5 shapes right now. Please enter one of the shapes provided.")
+        print("Invalid input. Please enter a valid shape name.")
+
+
 
